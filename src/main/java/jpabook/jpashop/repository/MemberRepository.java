973a54e9975bd652087c99e0c1e8b0d421328bbe
PMS <ms147850@gmail.com>
2022-07-14 00:59:37 +0900

    add MemberRepository.java

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;   //Q) EntityManager vs JpaRepository 상속 => 용도의 차이는??
+
+    //if EntityManagerFactory를 직접 주입하고 싶다면
+//    @PersistenceUnit
+//    private EntityManagerFactory emf;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select * from Member m", Member.class).getResultList();
+        //문법이 SQL과 약간 다름. JPQL 문법 사용할 것.
+        //엔티티 객체를 대상으로 쿼리를 한다고 생각하면 됨.
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select * from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
