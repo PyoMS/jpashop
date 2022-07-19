6ea55189d6e65d4763f3bbcbc60cec47a691aa0e
PMS <ms147850@gmail.com>
2022-07-19 22:19:38 +0900

    add updateItemForm.html

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -43,4 +45,35 @@ public class ItemController {
         return "items/itemList";
     }
 
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());   /** Book을 수정하는데 '엔티티'를 보내는게 아닌 중간 역할을 처리하는 'BookForm'을 보낸다. ( 가능하면 엔티티를 Controller에서는 다루지 않는다.)*/
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@PathVariable("itemId") Long itemId, @ModelAttribute("form") BookForm form) {
+        //준영속 객체 -> form을 통해 JPA에 한번 접근했었던 id값을 갖는 객체.
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+        /** 엔티티는 가능하면 Controller 단에서는 다루지 않게 유의한다.*/
+        itemService.updateItem(itemId, form.getPrice(), form.getName()); // 파라미터로 BookForm을 넘겨주지 않는 이유는 엔티티를 고유하게 보존하기 위해서.
+
+        return "redirect:/items";
+    }
 }
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,20 @@ public class ItemService {
     @Transactional  //상위 Transactional 설정 값을 오버라이딩 했다고 생각하면 됨. default readOnly = false
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    /**
+     * 변경 감지를 통한 update
+     */
+    @Transactional
+    public void updateItem(Long itemId, int price, String name) {
+        Item item = itemRepository.findOne(itemId); // 엔티티를 호출. -> 영속 상태 보존
+        item.setPrice(price);
+        item.setName(name);
+        /*itemRepository.save(item);*/    /** save 해줄필요가 없음!
+         @Transactional 이 commit 할 때, JPA에서 flush가 발생하는데 Dirty Checking을 해서
+         해당 변경사항이 있을 시 알아서 update 처리한 후 commit을 수행한다.
+         */
     }
 
     public List<Item> findItems(){
