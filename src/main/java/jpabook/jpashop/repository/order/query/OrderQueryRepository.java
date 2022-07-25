ffa08d24f9daaa244766ef976becca27f4f6475d
PMS <ms147850@gmail.com>
2022-07-25 22:13:51 +0900

    add 컬렉션 조회 v6

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -74,4 +74,25 @@ public class OrderQueryRepository {
     }
 
 
+    /**
+     * this.orderId = orderId;
+     *         this.name = name;
+     *         this.orderDate = orderDate;
+     *         this.orderStatus = orderStatus;
+     *         this.address = address;
+     *         this.itemName = itemName;
+     *         this.orderPrice = orderPrice;
+     *         this.count = count;
+     * */
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                        "select new " +
+                                " jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d" +
+                                " join o.orderItems oi" +
+                                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
